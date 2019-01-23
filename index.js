a3b7cc894551be4ce1595bc5d28de76d0ede32d5
Nosa Okundaye <okundaye.preciousnosa@gmail.com>
2019-01-23 04:23:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,12 +3,11 @@ const musicians = ["John Lennon", "Paul McCartney", "George Harrison", "Ringo St
     const instruments = ["Guitar", "Bass Guitar", "Lead Guitar", "Drums"];
   const facts= ["He was the last Beatle to learn to drive","He was never a vegetarian","He was a choir boy and boy scout","He hated the sound of his own voice"];
 function theBeatlesPlay(musicians, instruments){
-  var emptyArray= [], nArray= [];
+  var emptyArray= [];
   for(var i= 0; i< musicians.length; i++){
     emptyArray.push(musicians[0] + "plays" + instruments[0]);
-     nArray.push(musicians[i] + " plays " + instruments[i]);
   }
-  return nArray[i];
+  return emptyArray;
 }
 
 function johnLennonFacts(facts){
